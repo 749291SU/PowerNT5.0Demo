74f21ee16a9220eb82a2d4e9e61db61a7a2a450b
749291 <2014863935@qq.com>
2024-06-06 18:13:19 +0800

    first

---
+++ src/main/java/org/sici/Hello.java
@@ -0,0 +1,16 @@
+package org.sici;
+
+/**
+ * @projectName: PowerNT5.0Demo
+ * @package: org.sici
+ * @className: Hello
+ * @author: 749291
+ * @description: TODO
+ * @date: 6/6/2024 6:10 PM
+ * @version: 1.0
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+    }
+}
